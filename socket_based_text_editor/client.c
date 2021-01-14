c12b9f0b5e50c3ba0a20b99f2fa64d8fa027aa51
Ozan Çakmak <56032671+the-Ozzy@users.noreply.github.com>
2021-01-14 14:04:11 +0300

    Update and rename cli.c to client.c
    Added comments and translated interface to English

--- socket_based_text_editor/client.c
+++ socket_based_text_editor/client.c
@@ -11,45 +11,43 @@
 
 int main (void){
     int socket_descriptor, cli_sock_desc;
+    // opening the socket
     socket_descriptor = socket(AF_INET,SOCK_STREAM,0);
-
+    // checking about socket() function's state
     if (socket_descriptor < 0)
         return EXIT_FAILURE;
-    puts("soket acildi");
+    puts("Opening the socket is success");
     
+    // creating server's and child's id in server
     struct sockaddr_in server_addr, cli_addr;
     server_addr.sin_port = htons(2077);
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     char client_message[1024], server_message[1024];
+    // connecting to server
     if (connect(socket_descriptor,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0)
         return EXIT_FAILURE;
     while (true){
     memset(client_message, '\0',sizeof(client_message));
     memset(server_message, '\0',sizeof(server_message));
-    printf("Yazdirmak istediginiz bir cumleyi giriniz:\n\n\t");
+    // taking string from user
+    printf("Write your sentences and press ENTER\n\n->");
     fgets(client_message,1024,stdin);
     strtok(client_message,"\n");
-    
+    // checking for END keyword so that program cuts before other connections
     if(strcmp(client_message,"END") == 0){
         if (send(socket_descriptor,client_message,strlen(client_message),0) < 0)
             return EXIT_FAILURE;
         return EXIT_SUCCESS;
     }
+     // sending string to server
     if (send(socket_descriptor,client_message,strlen(client_message),0) < 0)
         return EXIT_FAILURE;
+     //getting string from server
     if (recv(socket_descriptor,server_message,strlen(server_message),0) < 0)
         return EXIT_FAILURE;
     
     printf("SERVER: %s\n",server_message);
     }
-    
-
-
-
-
-
-
-
-
+    return EXIT_SUCCESS;
 }
